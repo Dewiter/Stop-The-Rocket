ccc42de9e559b13fde7f0f7df33c62cdf6145ac3
Dewiter <robin.levy42@gmail.com>
2018-06-16 11:10:14 +0200

    pushed env models plus scripts

---
+++ Assets/Env/Script/consume.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class consume : MonoBehaviour {
+
+	private Renderer rend;
+	private float offset = -1f;
+	private float counter;
+	void	Start()
+	{
+		rend = GetComponent<Renderer>();
+		rend.material.shader = Shader.Find("graphs/Shader_diss");
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		counter += Time.deltaTime;
+		if (counter >= 5f)
+		{
+			if (offset <= 1f)
+				offset += .5f;	
+			counter = 0;
+		}
+		rend.material.SetFloat("Vector1_210D543D", offset);
+	}
+}
---
+++ Assets/Env/Script/hide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hide : MonoBehaviour {
+
+	public Material		surface;
+	private float		alphaHide = 0;
+	private Color		tmp;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			tmp = surface.color;
+			tmp.a -= 50;
+			surface.color = tmp;
+
+		}
+		if (Input.GetKeyUp(KeyCode.Tab))
+		{
+			tmp = surface.color;
+			tmp.a += 50;
+			surface.color = tmp;
+
+		}
+	}
+}
